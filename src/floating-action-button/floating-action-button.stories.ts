60959df57681e1ca8fe58130d3ccce45c5382d5e
Negarnbkhti42 <norob35@gmail.com>
2024-06-08 12:06:09 +0330

    fix: add FAB story to correct menu

--- src/floating-action-button/floating-action-button.stories.ts
+++ src/floating-action-button/floating-action-button.stories.ts
@@ -2,7 +2,7 @@ import { html, TemplateResult } from "lit";
 import "./index.js";
 
 export default {
-    title: 'Floating Action Button',
+    title: 'Components/Floating Action Button',
     component: 'tap-floating-action-button',
     args: {
         title: 'عنوان'
